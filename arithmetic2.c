ffc11018b5c008da86070d97fb8248e942fc0117
dewarshikalita <93310411+dewarshikalita@users.noreply.github.com>
2021-11-06 11:55:30 +0530

    Add files via upload

---
+++ arithmetic2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,s,w,d,m,x;
+	a=10;
+	b=2;
+	s=a+b;
+	printf("\nsum of %d and %d is %d",a,b,s);
+	w=a-b;
+	printf("\nsubstraction of %d to %d is %d",a,b,w);
+	d=a/b;
+	printf("\ndivision of %d by %d is %d",a,b,d);
+	m=a*b;
+	printf("\nmultiplication of %d into %d is %d",a,b,m);
+	x=a%b;
+	printf("\nmodulo division of %d and %d is %d",a,b,x);
+}
